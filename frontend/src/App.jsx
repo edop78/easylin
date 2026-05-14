f4ea8526c9dd4f045c5139fc12ad35c27c5dd1df
Edo <edop78@gmail.com>
2026-05-14 15:04:11 +0200

    Switched to MemoryRouter for static URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { MemoryRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 import Layout from './components/Layout/Layout';
 import Login from './pages/Login/Login';
