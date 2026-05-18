c3a73a5763ee75affd2a8eb56269e58f3be6c78f
Edo <edop78@gmail.com>
2026-05-18 13:27:44 +0200

    clean: remove security report and clean up unused diagnostic scratch files

--- scratch/view_chat.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('easylin.db')
-conn.row_factory = sqlite3.Row
-cursor = conn.cursor()
-res = conn.execute("SELECT * FROM chat_messages ORDER BY created_at DESC LIMIT 5").fetchall()
-for r in res:
-    print(dict(r))
-conn.close()
